80ccf636e5f2730fd3517a12419960993864a6e8
“mikeburf” <mjb2632@utulsa.edu>
2025-10-03 12:46:23 -0500

    avgmethods uses vec2 and type hints

--- avgmethods.py
+++ avgmethods.py
@@ -1,40 +1,28 @@
-# input vectors should be (x, y) tuples with magnitude <= 1
+from pyglet.math import Vec2
+# input vectors should be Vec2s
 
-def __sum(inputs):
-    total = 0
-    for i in inputs:
-        total += i
-    return total
+def __avg(inputs: list[Vec2]) -> Vec2:
+    return sum(inputs) / len(inputs)
 
-def __avg(inputs):
-    return __sum(inputs) / len(inputs)
-
-def __clamp(v):
-    mag = v[0]**2 + v[1]**2
+def __clamp(v: Vec2) -> Vec2:
+    mag = v.length_squared()
     if mag > 1:
         mag = mag**0.5
-        return (v[0]/mag, v[1]/mag)
+        return v / mag
     return v
 
-def __normalize(v):
-    mag = (v[0]**2 + v[1]**2)**0.5
-    if mag > 0:
-        return (v[0]/mag, v[1]/mag)
-    return (0, 0)
-
 #possible methods to use
-def sum_and_clamp(inputs):
-    return __clamp(__sum(inputs))
+def sum_and_clamp(inputs: list[Vec2]) -> Vec2:
+    return __clamp(sum(inputs))
 
-def sum_and_normalize(inputs):
-    return __normalize(__sum(inputs))
+def sum_and_normalize(inputs: list[Vec2]) -> Vec2:
+    return sum(inputs).normalize()
 
-def avg(inputs):
+def avg(inputs: list[Vec2]) -> Vec2:
     return __avg(inputs)
 
 # a little more complicated: basically its more sensitive, i think. it should be!
-def sum_norm_avg(inputs, number_moving):
-
+def sum_norm_avg(inputs: list[Vec2], number_moving: int) -> Vec2:
     return (number_moving / len(inputs)) * __avg(inputs)
 
 
