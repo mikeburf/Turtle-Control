9a0c5fa80af4c08f9c3ef5b1dd440ec379c432b1
mauratoney <156852328+mauratoney@users.noreply.github.com>
2025-10-07 19:15:41 -0500

    change everything again

--- mynode.py
+++ mynode.py
@@ -4,13 +4,14 @@ from pyglet.math import Vec2
 import pyglet.input as inp
 import threading
 import time
-import numpy
+import signal
 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import TwistStamped
 import rclpy
 from rclpy.clock import Clock
 from rclpy.qos import QoSProfile
+from rclpy.node import Node
 
 LINEAR_CONTROL_EXPONENT = 2.0 # how much to exponentiate the input direction by
 ANGULAR_CONTROL_EXPONENT = 1.5 # how much to exponentiate the input direction by
@@ -18,8 +19,88 @@ ANGULAR_CONTROL_EXPONENT = 1.5 # how much to exponentiate the input direction by
 MAX_VEL_LINEAR = 0.22
 MAX_VEL_ANGULAR = 3.0
 
-MAX_DELTAV_LINEAR = 0.02
-MAX_DELTAV_ANGULAR = 0.3
+MAX_DELTAV_LINEAR = 0.002
+MAX_DELTAV_ANGULAR = 0.03
+
+def clamp(x, minVal, maxVal):
+    if x < minVal: return minVal
+    if x > maxVal: return maxVal
+    return x
+
+def clamp_accel(current_v, target_v, max_delta):
+    if current_v < target_v:
+        return min(current_v + max_delta, target_v)
+    if current_v > target_v:
+        return max(current_v - max_delta, target_v)
+    return current_v
+
+class SigintSkipper:
+    def __init__(self, callback):
+        self.callback = callback
+    def __enter__(self):
+        self.got = False
+        self.handler_old = signal.signal(signal.SIGINT, self.handler)
+        
+
+    def handler(self, sig, frame):
+        self.got = (sig, frame)
+        self.callback()
+
+    def __exit__(self, type, value, traceback):
+        print("exiting sigint skipper")
+
+
+class TurtNode(Node):
+    def __init__(self, handler):
+        super().__init__('mynode')
+        self.handler = handler
+        self.qos = QoSProfile(depth=10)
+        self.pub = self.create_publisher(Twist, "cmd_vel", self.qos)
+
+        
+        self.target_linear_velocity = 0.0
+        self.target_angular_velocity = 0.0
+        self.control_linear_velocity = 0.0
+        self.control_angular_velocity = 0.0
+
+        self.timer = self.create_timer(0.01, self.loop)
+
+        self.shutting_down = False
+        #rclpy.get_default_context().on_shutdown(self.shutdown)
+
+    def move(self, linear, angular):
+        if not self.shutting_down:
+            twist = Twist()
+            twist.linear.x = linear
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = angular
+            self.pub.publish(twist)
+
+    def shutdown(self):
+        self.shutting_down = True
+        self.pub.publish(Twist())
+        print("node shutdown!")
+        self.get_clock().sleep_for(rclpy.duration.Duration(seconds=1))
+        print("node sleep?")
+
+    def loop(self):
+            
+        indir = self.handler.get_final_output()
+        self.target_linear_velocity = (abs(indir.y) ** LINEAR_CONTROL_EXPONENT) * MAX_VEL_LINEAR
+        self.target_angular_velocity = (abs(indir.x) ** ANGULAR_CONTROL_EXPONENT) * MAX_VEL_ANGULAR
+
+        if (indir.y > 0): self.target_linear_velocity *= -1
+        if (indir.x > 0): self.target_angular_velocity *= -1
+            
+        self.control_linear_velocity = clamp_accel(self.control_linear_velocity, self.target_linear_velocity, MAX_DELTAV_LINEAR)
+        self.control_angular_velocity = clamp_accel(self.control_angular_velocity, self.target_angular_velocity, MAX_DELTAV_ANGULAR)
+        self.move(self.control_linear_velocity, self.control_angular_velocity)
+
+        
 
 # this holds all the input data
 class InputHandler:
@@ -43,55 +124,6 @@ class InputHandler:
         for vec in self.inputs.values():
             total += vec
         return total / len(self.inputs)
-            
-    
-def clamp(x, min, max):
-    if x < min: return min
-    if x > max: return max
-    return x
-
-def clamp_accel(current_v, target_v, max_delta):
-    if current_v < target_v:
-        return min(current_v + max_delta, target_v)
-    if current_v > target_v:
-        return max(current_v - max_delta, target_v)
-    return current_v
-    
-
-def loop(handler, pub):
-
-    status = 0
-    target_linear_velocity = 0.0
-    target_angular_velocity = 0.0
-    control_linear_velocity = 0.0
-    control_angular_velocity = 0.0
-    try:
-        while (handler.running):
-            indir = handler.get_final_output()
-            target_linear_velocity = -numpy.sign(indir.y) * (indir.y ** LINEAR_CONTROL_EXPONENT) * MAX_VEL_LINEAR
-            target_angular_velocity = numpy.sign(indir.x) * (indir.x ** ANGULAR_CONTROL_EXPONENT) * MAX_VEL_ANGULAR
-
-            control_linear_velocity = clamp_accel(control_linear_velocity, target_linear_velocity, MAX_DELTAV_LINEAR)
-            control_angular_velocity = clamp_accel(control_angular_velocity, target_angular_velocity, MAX_DELTAV_ANGULAR)
-
-            twist = Twist()
-            twist.linear.x = control_linear_velocity
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
-            
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = control_angular_velocity
-
-            pub.publish(twist)
-
-    except Exception as e:
-        print("Thread stopped!", e)
-    finally:
-        pass
-            
-        
-
 
 # this uses the controller manager, which is more abstracted and therefore I dont trust it
 def main():
@@ -113,53 +145,42 @@ def main():
         handler.inputs.pop(controller)
         controller.remove_handlers(handler)
 
-    def debug_callback(dt):
-        print("\rCurrent Input:" + " "*50, handler.get_final_output())
-        handler.status += 1
+    def tick_callback(dt):
+        if not handler.running: pyglet.app.exit()
+        else:
+            print("Current Input:", handler.get_final_output())
+            handler.status += 1
+
 
     
     manager.on_connect = on_connect
     manager.on_disconnect = on_disconnect
 
-    pyglet.clock.schedule_interval(debug_callback, 1) # print the final output every second
+    pyglet.clock.schedule_interval(tick_callback, 1) # print the final output every second
 
     for controller in manager.get_controllers():
         on_connect(controller)
 
     rclpy.init()
+    node = TurtNode(handler)
 
-    #rclpy.on_shutdown(callback here)
-        
-    try:
-        print("Ctrl-C to quit")
-        qos = QoSProfile(depth=10)
-        node = rclpy.create_node('mynode')
-        pub = node.create_publisher(Twist, 'cmd_vel', qos)
-
-        
-        threading.Thread(target=loop, args=(handler, pub)).start()
-        print("Thread started")
-        pyglet.app.run() # runs the pyglet loop that handles input
-        print("Pyglet finished")
-    except KeyboardInterrupt:
-        """
-        twist = Twist()
-        twist.linear.x = 0.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 0.0
-        pub.publish(twist)
-        handler.running = False
-        """
-        pass
-    finally:
-        rclpy.try_shutdown()
+    
+    def finish_callback():
+        node.move(0.0, 0.0)
         node.destroy_node()
- 
-        #rclpy.shutdown()
+        rclpy.shutdown()
+        handler.running = False
+        
+
+    print("Ctrl-C to quit")
+
+    with SigintSkipper(finish_callback):
+        threading.Thread(target=pyglet.app.run, args=tuple()).start()
+        print("Thread started")
+        #pyglet.app.run() # runs the pyglet loop that handles input
+        rclpy.spin(node)
+        print("Pyglet finished")
+
 
     print ("done.")
 
